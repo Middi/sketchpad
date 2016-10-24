63fa38796027484dc94e3ef5def4b426e7db00b0
“Richard Middleton” <Richard@richardmiddleton.me>
2016-10-24 19:52:58 +0200

    created basic structure 16 x 16

--- jquery.js
+++ jquery.js
@@ -1,7 +1,30 @@
 $(document).ready(function() {
 
-    var numberDivs = $("#container").length;
+  var elementCount = "";
 
-    console.log(numberDivs);
+  var columnCount = "";
+
+//   function tableBuilder() {
+//
+//   if('.column').length < 16 {
+//
+//
+//
+//
+// }
+
+
+    for(elementCount = 1; elementCount < 17; elementCount++) {
+
+      $('#container').append('<div class="col"></div>');
+
+    }
+
+    for(columnCount = 1; columnCount < 17; columnCount++) {
+
+      $('.col').append('<div class="cell"></div>');
+
+    }
+
 
 });
