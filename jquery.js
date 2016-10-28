2e815e8234750825c1303bbaaa58d58f87644f55
“Richard Middleton” <Richard@richardmiddleton.me>
2016-10-28 17:48:27 +0200

    fixed reset grid

--- jquery.js
+++ jquery.js
@@ -1,50 +1,47 @@
-
 $(document).ready(function() {
 
 
-createGrid();
+    createGrid();
 
-  });
+});
 
 
 function createGrid() {
 
-  if('.cell'.length > 0) {
 
-    $(#container).remove()
+    // if('.cell'.length > 0) {
+    //
+    //   $(.cell).remove()
+    //
+    // }
 
-  }
+    var boxes = prompt("select a grid between 2 - 128?", "0");
 
 
-var boxes = prompt("select a grid between 2 - 128?","0");
+    var x = parseInt(boxes);
 
+    if (x > 128) {
 
-var x = parseInt(boxes);
-
-  if(x > 128) {
-
-    alert("must be between 1-128");
-  }
-
-    else {
+        alert("must be between 1-128");
+    } else {
 
 
     }
 
-    for(elementCount = 0; elementCount < x; elementCount++) {
+    for (elementCount = 0; elementCount < x; elementCount++) {
 
-      $('#container').append('<div class="col"></div>');
+        $('#container').append('<div class="col"></div>');
 
 
 
     }
 
-    for(columnCount = 0; columnCount < x; columnCount++) {
+    for (columnCount = 0; columnCount < x; columnCount++) {
 
-      $('.col').append('<div class="cell"></div>');
+        $('.col').append('<div class="cell"></div>');
 
-      $(".cell").width(572/x);
-      $(".cell").height(572/x);
+        $(".cell").width(572 / x);
+        $(".cell").height(572 / x);
     }
 
 
@@ -52,24 +49,56 @@ var x = parseInt(boxes);
 
 
 
-    $('.cell').mouseenter(function () {
-       $(this).addClass('highlight');
 
-   });
+    draw();
+
+}
+
+
+
+function draw() {
+
+
+        $('.cell').mouseenter(function() {
+            $(this).addClass('highlight');
+
+        });
 
 
 }
 
+
+
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+
+    $('.cell').mouseenter(function() {
+        $(this).css('background-color', 'color');
+
+    });
+
+}
 
 
 
 
 function clearButton() {
-  $(".cell").removeClass('highlight');
+    $(".cell").removeClass('highlight');
 
 }
 
-function resetButton() {
-  createGrid();
+function removeCell() {
+   $('#container').empty()
+}
 
+
+function resetButton() {
+    removeCell();
+    createGrid();
 }
